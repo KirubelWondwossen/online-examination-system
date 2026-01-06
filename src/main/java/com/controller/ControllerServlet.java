cddcf06785438cb99d30e3b34dce95debb3ec8cd
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2026-01-06 16:14:13 +0300

    Fix startup failure: remove duplicate servlet mapping annotation

--- src/main/java/com/controller/ControllerServlet.java
+++ src/main/java/com/controller/ControllerServlet.java
@@ -14,6 +14,10 @@ import java.sql.Timestamp;
 import java.util.Collections;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ControllerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
